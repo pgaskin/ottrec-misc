26a655c332955dbdcfec7157cecab594b88977f0
Patrick Gaskin <patrick@pgaskin.net>
2025-10-18 18:21:13 -0400

    validation/claudiff: filter inactive schedules

--- validation/claudiff.sh
+++ validation/claudiff.sh
@@ -22,7 +22,7 @@ jq -cr '
     with_entries(.key |= gsub(" ";"")) |
     with_entries(.value |= gsub("\\s+";" ")) |
     .Day |= ascii_downcase |
-    .RegistrationRequired = if .RegistrationRequired = "Yes" then "true" else if .RegistrationRequired = "No" then "false" else "" end end |
+    .RegistrationRequired = if .RegistrationRequired == "Yes" then "true" else if .RegistrationRequired == "No" then "false" else "" end end |
     [.FacilityName, .ActivityType, .Time, .Day, .RegistrationRequired] |
     join(" | ")
 ' |
@@ -30,18 +30,18 @@ iconv -f utf-8 -t ascii//translit -c |
 sort > $prefix.theirs.txt
 
 curl --fail -sL "https://data.ottrec.ca/export/$updated.json" |
-jq -cr '
+jq -cr --arg now "$updated" '
     INDEX(.facility[]; .url) as $f |
     .activity[] |
     .facilityName = $f[.facilityUrl].name |
     .rawActivity |= gsub(" *[*].*";"") |
     .time = .startTime + " - " + .endTime |
+    .current = (.startDate == null or .startDate <= $now) and ($now <= .endDate or .endDate == null) |
+    select(.current) |
     [.facilityName, .rawActivity, .time, .weekday, .reservationRequired] |
     join(" | ")
 ' |
 iconv -f utf-8 -t ascii//translit -c |
 sort > $prefix.ours.txt
-
-# TODO: do we need to filter ours for only currently active schedules?
 
 git diff --no-index $prefix.ours.txt $prefix.theirs.txt > $prefix.diff
