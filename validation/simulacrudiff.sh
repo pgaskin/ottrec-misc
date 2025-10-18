daf685a5e6b5946a325cf7025b7deb1de0a1836d
Patrick Gaskin <patrick@pgaskin.net>
2025-10-18 15:35:18 -0400

    validation/simulacrudiff: implement script for comparing data to simulacrumus/ottawa-drop-in-activity-scraper

---
+++ validation/simulacrudiff.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# This script compares data we've scraped to the data on simulacrumus's LLM-based scraper.
+#
+set -euxo pipefail
+cd "$(dirname "$0")"
+
+curl --fail -sL "https://github.com/simulacrumus/ottawa-drop-in-activity-scraper/raw/main/schedules.json" |
+jq -cr '
+    .[] |
+    .dayOfWeek = ["", "monday", "tuesday","wednesday","thursday","friday","saturday","sunday"][.dayOfWeek] |
+    [.facility, .activity, .startTime, .endTime, .dayOfWeek, .periodStartDate, .periodEndDate] |
+    join(" | ")
+' |
+iconv -f utf-8 -t ascii//translit -c |
+sort > theirs.txt
+
+curl --fail -sL "https://data.ottrec.ca/export/latest.json" |
+jq -cr '
+    INDEX(.facility[]; .url) as $f |
+    .activity[] |
+    .facilityName = $f[.facilityUrl].name |
+    .rawActivity |= gsub(" *[*].*";"") |
+    [.facilityName, .rawActivity, .startTime, .endTime, .weekday, .startDate, .endDate] |
+    join(" | ")
+' |
+iconv -f utf-8 -t ascii//translit -c |
+sort > ours.txt
+
+git diff --no-index ours.txt theirs.txt > diff.txt
